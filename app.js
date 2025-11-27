365b4def4940a05fc24d1fc044404a5fdd98ae64
Ashish Kumar <ashishkumar05164@gmail.com>
2025-11-27 18:51:17 +0530

    Directions action are defined

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const rows = Math.floor(board.clientHeight / blockHeight);
 const cols = Math.floor(board.clientWidth / blockWidth);
 
 const blocks = [];
-const snake = [{
-    x:1,
-    y: 3
-},{
-    x:1,
-    y:4
-},{
-    x:1,
-    y:5
-}];
+const snake = [
+    {
+    x:1, y: 3
+    },];
+
+
+let direction = "right ";
+
+
 for (let row = 0; row < rows; row++){
     for (let col = 0; col < cols; col++){
     const block = document.createElement('div'); //nested loop creates block
@@ -36,5 +35,29 @@ function render(){
 }
 
 setInterval(()=>{
+
+    let head = null;
+
+    if(direction === "left"){
+        head = {x:snake[0].x,y:snake[0].y-1}
+    }
+    else if(direction === "right"){
+        head = {x:snake[0].x,y:snake[0].y+1}
+    }
+    else if(direction === "up"){
+        head = {x:snake[0].x-1,y:snake[0].y}
+    }
+    else if(direction === "down"){
+        head = {x:snake[0].x+1,y:snake[0].y}
+    }
+
+    snake.forEach(segment =>{
+         blocks[`${segment.x}-${segment.y}`].classList.remove('fill')
+    })
+    
+    snake.unshift(head)
+    snake.pop()
+
+
     render()
-},300);
+},500);
